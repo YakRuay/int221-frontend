dc671fc840aa1f8405a5f32dcd62d5e1ffe8ed31
padnitri <57793978+padnitri@users.noreply.github.com>
2021-04-28 18:39:05 +0700

    styling main font:Promt

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
 
-createApp(App).use(router).mount('#app')
+// import Global Components
+import BaseNavbar from './components/BaseNavbar.vue'
+
+const app = createApp(App)
+app.component('base-navbar', BaseNavbar)
+
+app.use(router).mount('#app')
