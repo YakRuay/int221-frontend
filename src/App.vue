476030de27704e3765d57060861218cddeb7aaeb
padnitri <57793978+padnitri@users.noreply.github.com>
2021-04-30 03:21:33 +0700

    Done MockUp AboutPage

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template class="w-screen h-screen">
+  <title>YakRuay - Sandals Store</title>
   <link rel="preconnect" href="https://fonts.gstatic.com" />
   <link
     href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
@@ -14,11 +15,11 @@
 body {
   font-family: "Prompt", sans-serif;
 }
-h1 {
+/* h1 {
   padding: 3rem;
   font-weight: bold;
   font-family: "Prompt", sans-serif;
   color: brown;
   text-align: center;
-}
+} */
 </style>
--- src/views/Pages/AboutUs.vue
+++ src/views/Pages/AboutUs.vue
@@ -1,10 +1,50 @@
-<template>
-  <h1> This is our Story, our About Us page</h1>
+<template class="w-screen h-screen">
+  <title>YakRuay - Sandals Store</title>
+  <img
+    src="@/assets/Related-imgs/format-footwears.jpg"
+    class="max-h-96 lg:max-h-auto"
+    alt="YakRuay Sandals Store"
+  />
+  <h1 id="slogan">
+    " Thinking of <b>Sandals</b>, Thinking of <b>YakRuay</b> "
+  </h1>
+  <p class="lg:px-44 md:px-20 px-10 flex justify-center">
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+    non proident, sunt in culpa qui officia deserunt mollit anim id est
+    laborum."
+  </p>
+ 
+  <p class="lg:px-44 md:px-20 px-10 py-10 flex justify-center text-center text-xl text-yellow-500">
+    <b>
+      เล่าความเป็นมาทำไมเรามาเลือกขายรองเท้าจนมาเป็น YakRuay
+       <br /> // ไว้ค่อยคิดคอนเทนต์แล้วเขียนให้ ป.ล. heading นี้ทำหน้าระเบิด แต่ Final Version จะเอาออก
+    </b>
+  </p>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
+
+<style scoped>
+p {
+  font-family: "Prompt", sans-serif;
+}
+img {
+  min-width: 100vw;
+  overflow: hidden;
+}
+#slogan {
+  padding: 3rem;
+  font-size: 2.25rem;
+  line-height: 2.5rem;
+  font-family: "Prompt", sans-serif;
+  color: darkslategray;
+  text-align: center;
+}
+</style>
 
