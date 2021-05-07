932534a721f1ac99c87626f91cd7c1d3bf7f1ea3
pooripro <steampro35200@gmail.com>
2021-05-08 00:33:04 +0700

    [UPDATE] Form method validate not finished

--- src/views/Pages/TheForms.vue
+++ src/views/Pages/TheForms.vue
@@ -1,108 +1,262 @@
 <template >
-
-
   <h1>This is our Forms page <b> TheForms.vue</b></h1>
   <div class="p-3 flex justify-center w-screen">
-    <form class="for-forms">
+    <form @submit.prevent="validateForm" class="for-forms">
+      <!-- PRODUCT NAME -->
       <label for="product-name">PRODUCT NAME</label>
-      <input type="text" id="product-name" name="product-name" placeholder="PRODUCT NAME" />
-
+      <input
+        v-model="product.productName"
+        type="text"
+        id="product-name"
+        name="product-name"
+        placeholder="PRODUCT NAME"
+      />
+      <!-- MFD -->
       <label for="manu-date">MANUFACTURER DATE</label>
-      <input type="date" id="manu-date" name="manu-date"/>
-
+      <input
+        v-model="product.productReleaseDate"
+        type="date"
+        id="manu-date"
+        name="manu-date"
+      />
+      <!-- PRODUCT DESCRIPTION -->
       <label for="desc">PRODUCT DESCRIPTION</label>
-      <textarea type="text" id="desc" name="desc" placeholder="PRODUCT DESCRIPTION"/>
-
+      <textarea
+        v-model="product.productDetail"
+        type="text"
+        id="desc"
+        name="desc"
+        placeholder="PRODUCT DESCRIPTION"
+      />
+      <!-- BRAND -->
       <label for="brand" class="brand-heading"> BRAND </label>
-      <select name="brand" id="brand">
-        <option value="ADIDAS"> ADIDAS </option>
-        <option value="CROCS"> CROCS </option>
-        <option value="FILA"> FILA </option>
-    </select>
-
+      <select v-model="product.productBrand" name="brand" id="brand">
+        <option value="ADIDAS">ADIDAS</option>
+        <option value="CROCS">CROCS</option>
+        <option value="FILA">FILA</option>
+      </select>
+      <!-- IMAGE -->
       <div class="image-upload">
-        <label for="product-img" id="IMAGE-heading">IMAGE
-       <img src="@/assets/FormsImg/add-image.png" id="upload-pic"></label>
-        <input type="file" id="product-img" name="product-img"/> <br/>
+        <label for="product-img" id="IMAGE-heading"
+          >IMAGE <img src="@/assets/FormsImg/add-image.png" id="upload-pic"
+        /></label>
+        <input
+          @change="imageHandler"
+          type="file"
+          id="product-img"
+          name="product-img"
+          accept="image/png"
+        />
+        <br />
         <!-- <span id="click-here"> Click To Upload an Image. </span> -->
       </div>
-      
+      <!-- COLORS -->
       <div class="flex flex-col p-3 pl-0">
-      <span id="colors-heading">COLORS</span>
-      <div class="for-nextLine flex flex-row">
-        <label class="container">
-          <input type="checkbox" id="black" name="colors" value="black"/>
-          <span class="checkmark black"></span>
-        </label>
-        <label class="container"> 
-          <input type="checkbox" id="whiite" name="colors" value="white"/>
+        <span id="colors-heading">COLORS</span>
+        <div class="for-nextLine flex flex-row">
+          <label class="container">
+            <input
+              @click="colorHandler(1)"
+              type="checkbox"
+              id="black"
+              name="colors"
+              value="black"
+            />
+            <span class="checkmark black"></span>
+          </label>
+          <label class="container">
+            <input
+              @click="colorHandler(2)"
+              type="checkbox"
+              id="white"
+              name="colors"
+              value="white"
+            />
             <span class="checkmark white"></span>
-        </label>
-        <label class="container">
-          <input type="checkbox" id="brownish" name="colors" value="brownish"/>
-          <span class="checkmark brownish"></span>
-        </label>
-        <label class="container">  
-          <input type="checkbox" id="green" name="colors" value="green"/>
-          <span class="checkmark green"></span>
-        </label>
-        <label class="container">
-          <input type="checkbox" id="cardinal" name="colors" value="cardinal"/>
-          <span class="checkmark cardinal"></span>
-        </label>
-        <label class="container">  
-          <input type="checkbox" id="rhino" name="colors" value="rhino"/>
-          <span class="checkmark rhino"></span>
-        </label>
+          </label>
+          <label class="container">
+            <input
+              @click="colorHandler(3)"
+              type="checkbox"
+              id="brownish"
+              name="colors"
+              value="brownish"
+            />
+            <span class="checkmark brownish"></span>
+          </label>
+          <label class="container">
+            <input
+              @click="colorHandler(4)"
+              type="checkbox"
+              id="green"
+              name="colors"
+              value="green"
+            />
+            <span class="checkmark green"></span>
+          </label>
+          <label class="container">
+            <input
+              @click="colorHandler(5)"
+              type="checkbox"
+              id="cardinal"
+              name="colors"
+              value="cardinal"
+            />
+            <span class="checkmark cardinal"></span>
+          </label>
+          <label class="container">
+            <input
+              @click="colorHandler(6)"
+              type="checkbox"
+              id="rhino"
+              name="colors"
+              value="rhino"
+            />
+            <span class="checkmark rhino"></span>
+          </label>
         </div>
       </div>
-
-    <label for="price" class="price-heading">PRICE</label>
-    <input type="number" id="price" name="price" min="1" placeholder="PRICE" />
-
-    
-      <label class="warranty-heading">WARRANTY</label>
-    <div class="WARRANTY-BOX flex flex-col p-7 pt-0">
-      <div>
-      <input type="radio" id="none" name="warranty" value="none"/>
-      <label for="none" class="warranty-class"> none </label></div>
-      <div>
-      <input type="radio" id="7days" name="warranty" value="7days"/>
-      <label for="7days" class="warranty-class"> 7 days </label></div>
-      <div>
-      <input type="radio" id="14days" name="warranty" value="14days"/>
-      <label for="14days" class="warranty-class"> 14 days </label></div>
-   </div>
-
-     <div class="flex justify-end"> <input type="submit" value="Save" class="save-button"/> </div>
+      <!-- PRICE -->
+      <label for="price" class="price-heading">PRICE</label>
+      <input
+        v-model="product.productPrice"
+        type="number"
+        id="price"
+        name="price"
+        min="1"
+        placeholder="PRICE"
+      />
+      <!-- WARRANTY -->
+      <!-- <label class="warranty-heading">WARRANTY</label>
+      <div class="WARRANTY-BOX flex flex-col p-7 pt-0">
+        <div>
+          <input type="radio" id="none" name="warranty" value="none" />
+          <label for="none" class="warranty-class"> none </label>
+        </div>
+        <div>
+          <input type="radio" id="7days" name="warranty" value="7days" />
+          <label for="7days" class="warranty-class"> 7 days </label>
+        </div>
+        <div>
+          <input type="radio" id="14days" name="warranty" value="14days" />
+          <label for="14days" class="warranty-class"> 14 days </label>
+        </div>
+      </div> -->
+      <!-- SUBMIT -->
+      <div class="flex justify-end">
+        <input type="submit" value="Save" class="save-button" />
+      </div>
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      productValidate: false,
+      product: {
+        productName: "",
+        productReleaseDate: "",
+        productDetail: "",
+        productBrand: "",
+        productImage: "",
+        productColor: [],
+        productPrice: 0,
+      },
+    };
+  },
+
+  methods: {
+    validateForm() {
+      if (this.product.productName == "") {
+        this.productValidate = true;
+        alert("Please enter product name");
+      }
+      if (this.product.productReleaseDate == "") {
+        this.productValidate = true;
+        alert("Please enter MFD");
+      }
+      if (this.product.productDetail == "") {
+        this.productValidate = true;
+        alert("Please enter product description");
+      }
+      if (this.product.productBrand == "") {
+        this.productValidate = true;
+        alert("Please enter product brand");
+      }
+      if (this.product.productImage == "") {
+        this.productValidate = true;
+        alert("Please enter upload image");
+      }
+      if (this.product.productColor.length == 0) {
+        this.productValidate = true;
+        alert("Please enter product color");
+      }
+      if (this.productValidate == false) {
+        console.log("productValidate: " + this.productValidate);
+        this.addProduct();
+      }
+    },
+
+    addProduct() {
+      console.log("method: addProduct");
+    },
+
+    imageHandler(event) {
+      console.log("method: imageHandler");
+      console.log(event);
+      const input = event.target;
+      this.product.productImage = input.files[0].name;
+      console.log(this.product.productImage);
+      // this.product.productImage.push(file);
+      // this.previews.push(URL.createObjectURL(file));
+    },
+
+    colorHandler(colorID) {
+      let isFound = false;
+      for (let index = 0; index < this.product.productColor.length; index++) {
+        if (this.product.productColor[index] === colorID) {
+          this.product.productColor.splice(index, 1);
+          isFound = true;
+          break;
+        }
+      }
+      if (isFound === false) {
+        this.product.productColor.push(colorID);
+      }
+
+      console.log("productColor");
+      console.log(this.product.productColor);
+      console.log("length productColor: " + this.product.productColor.length);
+    },
+  },
+};
 </script>
 
 <style scoped>
-#colors-heading{
+#colors-heading {
   font-weight: bold;
   font-family: "Prompt", sans-serif;
   padding-top: 3%;
 }
 
 /* input IDs */
-#product-name,#manu-date,#desc,#price{
-  width:100%;
+#product-name,
+#manu-date,
+#desc,
+#price {
+  width: 100%;
 }
-#manu-date{
+#manu-date {
   color: gray;
 }
 
-label{
-  font-weight:bold;
+label {
+  font-weight: bold;
   font-family: "Prompt", sans-serif;
   display: block;
-  margin:  8% 1% 1% 0%;
+  margin: 8% 1% 1% 0%;
 }
 
 /* .price,.brand{
@@ -116,8 +270,9 @@ label{
 }
 
 input,
-textarea,select {
-  background-color:white;
+textarea,
+select {
+  background-color: white;
   font-weight: normal;
   font-family: "Prompt", sans-serif;
   border: 1px solid #73767a;
@@ -127,46 +282,45 @@ textarea,select {
   /* width: 80%; */
 }
 
-select:hover,textarea:hover,input:hover {
+select:hover,
+textarea:hover,
+input:hover {
   box-shadow: 2px 2px 8px #888888;
   border: 1px solid black;
 }
 
-.radio-tick{
+.radio-tick {
   padding: 0%;
 }
-textarea{
+textarea {
   height: 10%;
 }
-
 
 h1 {
   text-align: center;
 }
 
-#product-img::-webkit-file-upload-button
-{
-    display:none;
+#product-img::-webkit-file-upload-button {
+  display: none;
 }
-#product-img{
+#product-img {
   color: gray;
-  border:1px solid dimgray;
-  background-color:whitesmoke;
+  border: 1px solid dimgray;
+  background-color: whitesmoke;
   width: 70%;
   padding: 2%;
-  margin-top:0%;
+  margin-top: 0%;
 }
-#product-img:hover{
-  box-shadow:unset;
-}
-
-.image-upload img
-{
-    width: 1000px;
-    cursor: pointer;
+#product-img:hover {
+  box-shadow: unset;
 }
 
-#upload-pic{
+.image-upload img {
+  width: 1000px;
+  cursor: pointer;
+}
+
+#upload-pic {
   background-color: dimgray;
   border: 1px solid dimgray;
   box-sizing: border-box;
@@ -176,24 +330,24 @@ h1 {
   margin-bottom: 3%;
 }
 
-#upload-pic:hover{
+#upload-pic:hover {
   border-color: transparent;
   background-color: cadetblue;
   box-shadow: 2px 2px 8px dimgray;
 }
 
-#IMAGE-heading{
+#IMAGE-heading {
   width: 20%;
 }
 
-.warranty-class{
+.warranty-class {
   padding: 1%;
   margin: 2%;
   display: contents;
   font-weight: normal;
 }
 /* The container */
-.container{
+.container {
   width: 25px;
   flex-direction: column;
   display: block;
@@ -211,7 +365,7 @@ h1 {
 
 /* Hide the browser's default checkbox */
 .container input {
-  position:relative;
+  position: relative;
   opacity: 0;
   cursor: pointer;
   height: 0;
@@ -219,7 +373,7 @@ h1 {
   padding: 0%;
 }
 
-.for-nextLine{
+.for-nextLine {
   height: 25px;
   padding-top: 1%;
   padding-left: 5%;
@@ -242,19 +396,19 @@ h1 {
   height: 25px;
   width: 25px;
   background-color: white;
-  border:1px solid dimgray;
+  border: 1px solid dimgray;
 }
 .brownish {
   position: absolute;
   height: 25px;
   width: 25px;
-  background-color: #81785B;
+  background-color: #81785b;
 }
 .green {
   position: absolute;
   height: 25px;
   width: 25px;
-  background-color: #549A65;
+  background-color: #549a65;
 }
 .cardinal {
   position: absolute;
@@ -262,8 +416,7 @@ h1 {
   left: 0; */
   height: 25px;
   width: 25px;
-  background-color: #BF1B2F;
-
+  background-color: #bf1b2f;
 }
 .rhino {
   position: absolute;
@@ -271,7 +424,7 @@ h1 {
   left: 0; */
   height: 25px;
   width: 25px;
-  background-color:#343E61;
+  background-color: #343e61;
 }
 /* END OF CUSTOM CHECKBOX PER COLORS */
 
@@ -279,7 +432,6 @@ h1 {
 .container:hover input ~ .checkmark {
   background-color: #ccc;
 }
-
 
 /* Create the checkmark/indicator (hidden when not checked) */
 .checkmark:after {
@@ -305,7 +457,7 @@ h1 {
   -ms-transform: rotate(45deg);
   transform: rotate(45deg);
 }
- /* for White color check (Black tick) */
+/* for White color check (Black tick) */
 .container .white:after {
   left: 9px;
   top: 5px;
@@ -318,49 +470,49 @@ h1 {
   transform: rotate(45deg);
 }
 
-.save-button{
+.save-button {
   width: 75px;
   color: white;
-  background: #10B981;
+  background: #10b981;
   border-radius: 5px;
   border-color: transparent;
   cursor: pointer;
 }
 
-.save-button:hover{
+.save-button:hover {
   background-color: cadetblue;
   border-color: transparent;
   box-shadow: 2px 2px 8px dimgray;
 }
 
-@media (max-width:768px) {
+@media (max-width: 768px) {
   .for-forms {
-  width: 90vw;
-  padding-bottom: 15%;
-}
-label,span{
-  font-size: 0.875rem;
-  line-height: 1.25rem;
-}
-.image-upload img
-{
+    width: 90vw;
+    padding-bottom: 15%;
+  }
+  label,
+  span {
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+  }
+  .image-upload img {
     cursor: pointer;
+  }
+  #product-img {
+    width: 95%;
+  }
+  #IMAGE-heading {
+    width: 25%;
+  }
 }
-#product-img{
-  width: 95%;
-}
-#IMAGE-heading{
-  width: 25%;
-}
-}
-@media (width:768px) {
+@media (width: 768px) {
   .for-forms {
-  width: 60%;
+    width: 60%;
+  }
 }
-}
-@media (min-width:1024px) {
+@media (min-width: 1024px) {
   .for-forms {
-  width: 40%;
-}
+    width: 40%;
+  }
 }
 </style>
